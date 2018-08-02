8ca29670d0d2e339675dcdd2c01dac2ec90db6a2
Mathspy <mathspy257@gmail.com>
2018-08-03 01:04:19 +0200

    Added Storybook Viewport add-on, I love it!

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,34 @@
 import { configure } from "@storybook/react";
+import {
+  configureViewport,
+  INITIAL_VIEWPORTS,
+} from "@storybook/addon-viewport";
+
+const newViewports = {
+  iphonex: {
+    name: "iPhone X",
+    styles: {
+      width: "375px",
+      height: "812px",
+    },
+    type: "mobile",
+  },
+  smalldesktop: {
+    name: "1366 desktop",
+    styles: {
+      width: "1366px",
+      height: "768px",
+    },
+    type: "dekstop",
+  },
+};
+
+configureViewport({
+  viewports: {
+    ...INITIAL_VIEWPORTS,
+    ...newViewports,
+  },
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../stories", true, /.stories.js$/);
