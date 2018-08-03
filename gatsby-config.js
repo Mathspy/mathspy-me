8c4397ae3173f9e843fb2f57b946953a1c52a314
Mathspy <mathspy257@gmail.com>
2018-08-03 11:43:47 +0200

    Added and setup Gatsby Remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,28 @@ module.exports = {
   siteMetadata: {
     title: "Mathspy",
   },
-  plugins: ["gatsby-plugin-react-helmet"],
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `${__dirname}/content/`,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-prismjs",
+            options: {
+              classPrefix: "prism-",
+            },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+  ],
 };
