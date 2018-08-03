18c26fddb260c1db385e4cb71292fe8fc3874dfd
Mathspy <mathspy257@gmail.com>
2018-08-03 17:33:46 +0200

    CircleCI update v2
    I didn't realise Netlify is going to build for me on its own.
    Removed CircleCI's building. As well as fixed a formating error

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import Layout from "../components/Layout";
 const IndexPage = () => (
   <Layout>
     <h1>
-      This website is a heavily a WIP (Work in progress) all feedback is welcome!
+      This website is a heavily a WIP (Work in progress) all feedback is
+      welcome!
     </h1>
   </Layout>
 );
