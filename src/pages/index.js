a145f8a282911cab98a576cba229c1078374de4c
Mathspy <mathspy257@gmail.com>
2018-11-23 22:20:15 +0200

    Changed homepage into old homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout>
-    <h1>
-      This website is a heavily a WIP (Work in progress) all feedback is
-      welcome!
-    </h1>
+    <h1>Hi. Sorry, I am still under development! Visit me later! <svg height="52px" xmlns="http://www.w3.org/2000/svg" version="1" viewBox="0 0 645 585">
+      <path d="M297.3 550.9a667.9 667.9 0 0 0-76.4-67c-83.8-63.1-95.2-72.3-129.2-103.6C29 322.6 2.4 264.6 2.5 186c0-38.4 2.7-53.2 13.4-75.8 18.3-38.4 45.1-67 79.5-84.4 24.3-12.4 36.3-17.8 76.9-18 42.5-.3 51.4 4.6 76.4 18.4 30.5 16.7 61.8 52.4 68.3 77.8l4 15.7 9.8-21.6c55.7-122 233.6-120.2 295.5 3 19.7 39 21.8 122.5 4.4 169.5-22.7 61.3-65.4 108-164 179.7-64.7 47-137.9 118-143 128-6 11.6-.3 1.8-26.4-27.4z" fill="#FFF" />
+    </svg></h1>
   </Layout>
 );
+
+
 
 export default IndexPage;
