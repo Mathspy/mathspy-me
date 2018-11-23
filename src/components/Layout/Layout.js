2e9ef5a175b0fef33185879184681c757ac33821
Mathspy <mathspy257@gmail.com>
2018-11-23 22:19:59 +0200

    Removed Layout's elements

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { css } from "emotion";
 import Helmet from "react-helmet";
 
-import NavBar from "../NavBar";
-import Footer from "../Footer";
-
-import MathspyIcon from "../static/MathspyIcon";
-import ProjectsIcon from "../static/ProjectsIcon";
-import BlogIcon from "../static/BlogIcon";
-
-const Layout = ({ data, children, pathname }) => (
+const Layout = ({ data, children }) => (
   <>
     <Helmet
       title={data.site.siteMetadata.title}
@@ -18,48 +10,10 @@ const Layout = ({ data, children, pathname }) => (
         { name: "keywords", content: "mathspy, blog" },
       ]}
     />
-    <div className={outterLayoutStyles}>
-      <div className={innerLayoutStyles}>
-        <NavBar>
-          <MathspyIcon />
-          <ProjectsIcon />
-          <BlogIcon />
-        </NavBar>
-        <div className={contentStyles}>{children}</div>
-      </div>
-      <Footer pathname={pathname} />
+    <div>
+      {children}
     </div>
   </>
 );
-
-const innerLayoutStyles = css`
-  display: flex;
-  flex-direction: column;
-  flex: 10;
-
-  @media (min-width: 600px) {
-    flex-direction: row;
-  }
-`;
-
-const outterLayoutStyles = css`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-
-  @media (min-width: 420px) {
-    justify-content: space-between;
-  }
-
-  @media (min-width: 900px) {
-    flex-direction: row;
-  }
-`;
-
-const contentStyles = css`
-  @media (min-width: 900px) {
-    flex: 1 768px;
-  }
-`;
 
 export default Layout;
